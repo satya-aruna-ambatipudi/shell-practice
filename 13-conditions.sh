bc20b7660f307bb59016e75cd319f37ed31ad185
Satya Aruna Ambatipudi <journeywithin.satya@gmail.com>
2026-08-14 17:16:36 +0530

    new script added

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -15,17 +15,17 @@ else
 fi
 
 if [[ $NUMBER -gt 10  ]]; then
-    echo "Given number: $NUMBER is greater than 20"
+    echo "Given number: $NUMBER is greater than 10"
 elif [[ $NUMBER -eq 10 ]]; then
-    echo "Given number: $NUMBER is equal to 20"
+    echo "Given number: $NUMBER is equal to 10"
 else
-    echo "Given number: $NUMBER is less than 20"
+    echo "Given number: $NUMBER is less than 10"
 fi
 
 if (( $NUMBER > 15 )); then
-    echo "Given number: $NUMBER is greater than 20"
+    echo "Given number: $NUMBER is greater than 15"
 elif (( $NUMBER = 15 )); then
-    echo "Given number: $NUMBER is equal to 20"
+    echo "Given number: $NUMBER is equal to 15"
 else
-    echo "Given number: $NUMBER is less than 20"
+    echo "Given number: $NUMBER is less than 15"
 fi
