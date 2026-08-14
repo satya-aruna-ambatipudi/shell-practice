9447c738e48ba8d86ab9994726e7fd3f8c9823ea
Satya Aruna Ambatipudi <journeywithin.satya@gmail.com>
2026-08-14 12:29:58 +0530

    new script added

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=Suresh
+PERSON2=Ramesh
+echo "$PERSON1: Hi $PERSON2, How are you?"
+echo "$PERSON2: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1: I am good. Where are you going?"
+echo "$PERSON2: I am going to supermarket."
