9e5f18913848140c86fbad73539ba898f4bbabb9
Satya Aruna Ambatipudi <journeywithin.satya@gmail.com>
2026-08-14 13:41:44 +0530

    new script added

--- 09-decimalmath.sh
+++ 09-decimalmath.sh
@@ -5,6 +5,14 @@ total_memory=16
 used_memory=7
 
 # Capture decimal output using command substitution
-mem_percentage=$(echo "scale=2; ($used_memory / $total_memory) * 100" | bc)
+#mem_percentage=$(echo "scale=2; ($used_memory / $total_memory) * 100" | bc)
+
+# the above command won't work if bc is not installed in your linux environment
+
+# FIX: Use awk instead of bc for floating-point calculations
+mem_percentage=$(awk -v u="$used_memory" -v t="$total_memory" 'BEGIN { printf "%.2f", (u / t) * 100 }')
 
 echo "Memory usage is: $mem_percentage%" # Outputs: 43.75%
+
+
+
