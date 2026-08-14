9b2a405d552e9f77eb8eb6f84fdd7383bfc6c882
Satya Aruna Ambatipudi <journeywithin.satya@gmail.com>
2026-08-14 12:36:39 +0530

    new script added

--- 04-parameters.sh
+++ 04-parameters.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=S$1
-PERSON2=R$2
+PERSON1=$1
+PERSON2=$2
 echo "$PERSON1: Hi $PERSON2, How are you?"
 echo "$PERSON2: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1: I am good. Where are you going?"
