700b9dcfa4d62bbb5fa47f2cd64e44b7c6431ac4
Satya Aruna Ambatipudi <journeywithin.satya@gmail.com>
2026-08-14 12:14:40 +0530

    conversation created

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh: Hi Ramesh, How are you?"
+echo "Ramesh: Hi Suresh, I am fine. How are you?"
+echo "Suresh: I am good. Where are you going?"
+echo "Ramesh: I am going to supermarket."
