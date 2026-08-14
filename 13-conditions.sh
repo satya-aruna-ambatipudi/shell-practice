8496bb2a9ed45d005f25e5135f183290608836d1
Satya Aruna Ambatipudi <journeywithin.satya@gmail.com>
2026-08-14 17:15:21 +0530

    new script added

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -21,3 +21,11 @@ elif [[ $NUMBER -eq 10 ]]; then
 else
     echo "Given number: $NUMBER is less than 20"
 fi
+
+if (( $NUMBER > 15 )); then
+    echo "Given number: $NUMBER is greater than 20"
+elif (( $NUMBER = 15 )); then
+    echo "Given number: $NUMBER is equal to 20"
+else
+    echo "Given number: $NUMBER is less than 20"
+fi
