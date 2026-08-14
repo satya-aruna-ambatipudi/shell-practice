a59bddbcbec5d56f4c1b92829c87038649b74212
Satya Aruna Ambatipudi <journeywithin.satya@gmail.com>
2026-08-14 13:36:49 +0530

    new script added

--- 10-decimalmath.sh
+++ 10-decimalmath.sh
@@ -5,6 +5,6 @@ result1=$(awk 'BEGIN {print 5.5 * 2.1}')
 
 echo "Result1: $result1" # Outputs: 11.55
 
-result2=$(awk '{print 8.0 / 3.5}')
+result2=$(awk 'BEGIN {print 8.0 / 3.5}')
 
 echo "Result2: $result2"
