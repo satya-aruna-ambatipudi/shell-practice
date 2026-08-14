92807fcd2b1dbdc1e98ee7c454dc9535648c3bbe
Satya Aruna Ambatipudi <journeywithin.satya@gmail.com>
2026-08-14 13:08:11 +0530

    new script added

--- 07-mathexpressions.sh
+++ 07-mathexpressions.sh
@@ -4,6 +4,7 @@ STARTTIME=$(date +%s)
 echo "Script start time $(date)"
 sleep 10
 ENDTIME=$(date +%s)
-TOTALTIME=$ENDTIME-$STARTTIME
+#TOTALTIME=$ENDTIME-$STARTTIME
+TOTALTIME=$(ENDTIME-STARTTIME)
 echo "Script end time $(date)"
 echo "Execution time $TOTALTIME"
