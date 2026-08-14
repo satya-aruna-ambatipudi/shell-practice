8353bfab5ea8918fd19d7aed0d8486e72e7366c6
Satya Aruna Ambatipudi <journeywithin.satya@gmail.com>
2026-08-14 13:33:36 +0530

    new script added

---
+++ 10-decimalmath.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Capture math output using awk
+result1=$(awk 'BEGIN {print 5.5 * 2.1}')
+
+echo "Result1: $result1" # Outputs: 11.55
+
+result2=$(awk '{print 8.0 / 3.5}')
+
+echo "Result2: $result2"
